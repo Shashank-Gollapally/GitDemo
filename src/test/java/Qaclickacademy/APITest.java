37ab5f5ae59b777a3c42aaea239603ce7d7131e9
shashank <shashankbabu69@gmail.com>
2021-05-13 21:06:19 +0530

    my first commit

---
+++ src/test/java/Qaclickacademy/APITest.java
@@ -0,0 +1,13 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	public void apitest()
+	{
+		System.out.println("apitest");
+	}
+
+}
---
+++ src/test/java/Qaclickacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserinvocation() {
+		System.out.println("browserinvocation");
+	}
+	
+	@Test
+	public void urldow() {
+		System.out.println("urldow");
+	}
+	}
