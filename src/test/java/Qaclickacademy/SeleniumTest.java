5dcf2b9c2ee505efc171ef0cf994d59fc0ad6e6f
shashank <shashankbabu69@gmail.com>
2021-05-13 22:02:52 +0530

    the new branch

--- src/test/java/Qaclickacademy/SeleniumTest.java
+++ src/test/java/Qaclickacademy/SeleniumTest.java
@@ -9,10 +9,16 @@ public class SeleniumTest {
 		System.out.println("browserinvocation");
 		System.out.println("the new added line");
 		System.out.println("the new  2nd added line");
+		System.out.println("the new branch ");
 	}
 	
 	@Test
 	public void urldow() {
 		System.out.println("urldow");
 	}
+	@Test
+	public void urldow1() {
+		System.out.println("urldow");
+	}
+	
 	}
